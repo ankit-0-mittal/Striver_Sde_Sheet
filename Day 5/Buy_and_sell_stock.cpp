3adabd473696f8a6be2d07e1232c289e18ab73bb
ankit-0-mittal <91519258+ankit-0-mittal@users.noreply.github.com>
2022-06-09 18:06:22 +0530

    Create Buy_and_sell_stock.cpp

---
+++ Day 5/Buy_and_sell_stock.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+
+   int mini = 10E9,profit = 0;
+
+   for(auto it:prices){
+
+     int temp = it - mini;
+
+     if(temp<=0)mini = it;
+
+     profit = max(profit,temp);
+
+   }
+
+   return profit;
+
+}
