a9cca6919c0942b0d8b89c78ad442b1f04233909
ankit-0-mittal <91519258+ankit-0-mittal@users.noreply.github.com>
2022-07-08 19:51:00 +0530

    Create Floor_BST.cpp

---
+++ Day 33/Floor_BST.cpp
@@ -0,0 +1,35 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+int floorInBST(TreeNode<int> * root, int X)
+{
+    auto curr = root;
+    int ans = -1;
+    
+    while(curr) {
+        if(curr -> val > X) {
+            curr = curr -> left;
+        } else {
+            ans = curr -> val;
+            curr = curr -> right;
+        }
+    }
+    
+    return ans;
+}
